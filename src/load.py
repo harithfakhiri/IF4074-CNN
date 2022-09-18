d375a7cf834598133b4e899f6159ec8f8a72834f
harithfakhiri <13519161@std.stei.itb.ac.id>
2022-09-18 13:32:19 +0700

    tinggal main harusnya

--- src/load.py
+++ src/load.py
@@ -10,7 +10,6 @@ from numpy import asarray
 from PIL import Image
 
 root = os.path.dirname(os.path.abspath(__file__)) +'/../dataset'
-print(root)
 
 # change the working directory to the path where the images are located
 def createPath(root, animal):
@@ -35,27 +34,18 @@ def loadDataSet(root, animal):
 def createMatrix(path,animal_arr,idx):
     # print(animal_arr)
     image = Image.open(path+'/'+animal_arr[idx])
-    print(animal_arr[idx])
+    # print(animal_arr[idx])
     # convert image to numpy array
-    print(image)
+    # print(image)
     data = asarray(image)
-    print(type(data))
+    # print(type(data))
     # summarize shape
     print(data.shape)
 
-    # create Pillow image
-    image2 = Image.fromarray(data)
-    print(type(image2))
-
-    # summarize image details
-    print(image2.mode)
-    print(image2.size)
-    print(data)
-    print('====')
-    print("red only")
-    red = createOneColor(data, 0)
-    green = createOneColor(data, 1)
-    blue = createOneColor(data, 2)
+    squared = squaredPadding(data)
+    red = createOneColor(squared, 0)
+    green = createOneColor(squared, 1)
+    blue = createOneColor(squared, 2)
 
 def createRedOnly(animal_arr):
     for i in range(len(animal_arr)):
@@ -63,26 +53,37 @@ def createRedOnly(animal_arr):
             for k in range(len(animal_arr[0][0])):
                 if(k!=0):
                     animal_arr[i][j][k] = 0
-    # print(animal_arr)
     return animal_arr
 
 def createOneColor(color_one,idx):
     height_px = len(color_one)
     width_px = len(color_one[0])
-    # red = [[0 for c in range(width_px)] for r in range(height_px)]
     red = np.zeros((height_px, width_px), dtype=int)
     for i in range(height_px):
         for j in range(width_px):
             red[i][j] = color_one[i][j][idx]
     return red
     
-def squaredPadding(color_one):
-    width, height = color_one.size[0], color_one.size[1]
+def squaredPadding(RGB_Matrix):
+    height, width = RGB_Matrix.shape[0], RGB_Matrix.shape[1]
     matrix = np.zeros((height,width,3),dtype=int)
+    if (height>width):
+        new_height = height
+        new_width = height
+    else : 
+        new_height = width
+        new_width = width
+    padding_h = int((new_height - height)/2)
+    padding_w = int((new_width - width)/2)
+    print(padding_h, padding_w)
+    print(new_height, new_width)
+    matrix = np.zeros((new_height,new_width,3),dtype=int)
+    #inserting img to zeros
+    matrix[padding_h:height + padding_h, padding_w:width + padding_w] = RGB_Matrix
+    print(matrix)
     return matrix
     
 
 dogs = loadDataSet(root, 'dogs')
 cats = loadDataSet(root, 'cats')
 createMatrix(createPath(root,'dogs'), dogs, 0)
-print(squaredPadding(dogs))
